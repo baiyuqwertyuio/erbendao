5811abe88a6dd983dfad2e2f0db649e814eb85f9
29796 <297966645@qq.com>
2020-12-14 17:47:42 +0800

    登陆注册接口

--- src/main/java/com/qf/controller/UserController.java
+++ src/main/java/com/qf/controller/UserController.java
@@ -1,4 +1,36 @@
 package com.qf.controller;
 
+import com.qf.common.BaseResp;
+import com.qf.pojo.User;
+import com.qf.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value = "/sendMail",method = RequestMethod.POST)
+    public BaseResp sendMail(@RequestBody Map map){
+        String email = (String)map.get("email");
+
+        return userService.send(email);
+    }
+
+    @RequestMapping(value = "/registry",method = RequestMethod.POST)
+    public BaseResp registry(@RequestBody User user){
+        return userService.registry(user);
+    }
+
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    public BaseResp login(@RequestBody User user){
+        return userService.login(user);
+    }
+
+
 }
--- src/main/java/com/qf/pojo/User.java
+++ src/main/java/com/qf/pojo/User.java
@@ -1,4 +1,25 @@
 package com.qf.pojo;
 
+import lombok.Data;
+
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "email")
+    private String email;
+
+    private String userName;
+
+    private String code;
+
+    private String password;
+
 }
